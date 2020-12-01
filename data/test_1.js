bdf2fa3bb065eb98eb10b2b36ad9139f6d7df423
Gavraa <75287032+Gavraa@users.noreply.github.com>
2020-12-01 21:05:20 +0700

    Add files via upload

---
+++ data/test_1.js
@@ -0,0 +1,8 @@
+var json_test_1 = {
+"type": "FeatureCollection",
+"name": "test_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 109.800384072692481, -7.725015093547006 ] } }
+]
+}
